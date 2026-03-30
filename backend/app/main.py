906d0907e11e3e2b82e6352e500e6bf5fd81021a
Pedro Yuri <pedroyurirn0@gmail.com>
2026-03-29 22:09:49 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,27 @@
+import logging
+import os
+
+import firebase_admin
+from firebase_admin import credentials
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import settings
-from app.routes import catalog, auth
 from app.middleware.logging_middleware import LoggingMiddleware
-import logging
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
+if not firebase_admin._apps:
+    cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
+    firebase_admin.initialize_app(cred, {
+        "projectId": os.getenv("GCP_PROJECT_ID"),
+        "storageBucket": f"{os.getenv('GCP_PROJECT_ID')}.firebasestorage.app",
+    })
+
+from app.routes import catalog, auth, upload  
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
@@ -29,6 +41,7 @@ app.add_middleware(LoggingMiddleware)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(catalog.router, prefix="/catalog", tags=["catalog"])
+app.include_router(upload.router)
 
 
 @app.get("/health", tags=["health"])
